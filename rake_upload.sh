8b0dd781ae9b4363864aa5021d959a08b63557bd
Dennis Vink <dennisvink@binx.io>
2020-03-20 02:57:30 +0100

    Allow AWS credentials to be passed to Docker

--- rake_upload.sh
+++ rake_upload.sh
@@ -1,1 +1,1 @@
-docker run -v $PWD:/stack -w /stack/ -e ENVIRONMENT masterofcloud/compiler:latest /bin/bash -c "rake upload"
+docker run -v $PWD:/stack -w /stack/ -e ENVIRONMENT -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY masterofcloud/compiler:latest /bin/bash -c "rake upload"
