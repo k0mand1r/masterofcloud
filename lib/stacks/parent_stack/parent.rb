959323084def43424936d1654e8b5356e6aa3dda
Dennis Vink <dennisvink@binx.io>
2020-03-26 19:55:21 +0100

    Add parent stacks

--- lib/stacks/parent_stack/parent.rb
+++ lib/stacks/parent_stack/parent.rb
@@ -12,6 +12,32 @@ module InfraStack
         r.property(:tags) { default_tags }
       end
 
+      resource :acm_stack,
+               type: "AWS::CloudFormation::Stack" do |r|
+        r.property(:template_url) { "acmstack" }
+        r.property(:tags) { default_tags }
+      end
+
+      resource :ecs_stack,
+               type: "AWS::CloudFormation::Stack" do |r|
+        r.property(:template_url) { "ecsstack" }
+        r.property(:parameters) do
+          {
+            "Vpc": "VpcStack".ref("Outputs.VpcId"),
+            "Subnets": [
+              :vpc_stack.ref("Outputs.Ec2PrivateSubnetName"),
+              :vpc_stack.ref("Outputs.Ec2PrivateSubnet2Name"),
+              :vpc_stack.ref("Outputs.Ec2PrivateSubnet3Name")
+            ].fnjoin(","),
+            "PublicSubnets": [
+              :vpc_stack.ref("Outputs.Ec2PublicSubnetName"),
+              :vpc_stack.ref("Outputs.Ec2PublicSubnet2Name"),
+              :vpc_stack.ref("Outputs.Ec2PublicSubnet3Name")
+            ].fnjoin(",")
+          }
+        end
+      end
+
       create_applications
     end
   end
