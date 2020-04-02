cbf0bab376e5b26a7e2cf0a6dd02282d4598114a
Dennis Vink <dennisvink@binx.io>
2020-04-02 22:29:59 +0200

    Satisfy rubocop

--- lib/stacks/lambda_stack/lambda_function.rb
+++ lib/stacks/lambda_stack/lambda_function.rb
@@ -3,7 +3,7 @@ module LambdaStack
     extend ActiveSupport::Concern
     included do
       # https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-resource-function.html
-      transform # This keyword enables CloudFormation transformations. In essence, it allows for the use of Serverless Framework (SAM) in CFN.  
+      transform # This keyword enables CloudFormation transformations. In essence, it allows for the use of Serverless Framework (SAM) in CFN.
 
       lambda_function_code = file_to_inline("lib/lambdas/post_to_slack.py") # Useful method for storing code as a single String
 
